0449f621dfac4193d257c1beb4cea9f2cff3bc23
Andrew Loozya <40801353+andrew-lozoya@users.noreply.github.com>
2024-01-30 13:32:58 -0600

    Update newrelicinstrumentation.ts
    remvoal of superagent, koa

--- autoinstrumentation/nodejs/src/newrelicinstrumentation.ts
+++ autoinstrumentation/nodejs/src/newrelicinstrumentation.ts
@@ -1,7 +1,5 @@
 const newrelic = require('newrelic');
 const awsSdk = require('@newrelic/aws-sdk');
-const koa = require('@newrelic/koa');
-const superagent = require('@newrelic/superagent');
 const nativeMetrics = require('@newrelic/native-metrics');
 
-export { newrelic, awsSdk, koa, superagent, nativeMetrics };
+export { newrelic, awsSdk, nativeMetrics };
