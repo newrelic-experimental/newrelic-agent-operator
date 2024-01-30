00af38b42503b28900faf13d83412061b9128ed0
Andrew Lozoya <lozoyaatl@att.net>
2024-01-30 14:11:50 -0600

    removal of @newrelic/aws-sdk

--- autoinstrumentation/nodejs/src/newrelicinstrumentation.ts
+++ autoinstrumentation/nodejs/src/newrelicinstrumentation.ts
@@ -1,5 +1,4 @@
 const newrelic = require('newrelic');
-const awsSdk = require('@newrelic/aws-sdk');
 const nativeMetrics = require('@newrelic/native-metrics');
 
-export { newrelic, awsSdk, nativeMetrics };
+export { newrelic, nativeMetrics };
